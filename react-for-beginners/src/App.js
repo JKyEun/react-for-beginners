fed00a88c630b937a3bb76fd2d936a8a9a9252cd
JKyEun <j56237@naver.com>
2023-03-09 09:06:56 +0900

    useEffect 배우기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,28 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  useEffect(() => {
+    console.log(`Keyword Changes! ${keyword}`);
+  }, [keyword]);
+  useEffect(() => {
+    console.log(`Counter Changes! ${counter}`);
+  }, [counter]);
+  useEffect(() => {
+    console.log(`Keyword or Counter Changes!`);
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Continue'} />
+      <input type='text' onChange={onChange} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
