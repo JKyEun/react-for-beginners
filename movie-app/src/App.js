5c73e0bde04d5d95c0e721cec1e71e873f81ef6d
JKyEun <j56237@naver.com>
2023-03-19 09:06:55 +0900

    movie-app api 통해 영화 정보 가져오기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading . . .</h1>
+      ) : (
+        movies.map((el) => {
+          return (
+            <div key={el.id}>
+              <h2>{el.title}</h2>
+              <img src={el.medium_cover_image} alt='영화 포스터' />
+              <p>{el.summary}</p>
+              <ul>
+                {el.genres.map((el) => (
+                  <li key={el}>{el}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
