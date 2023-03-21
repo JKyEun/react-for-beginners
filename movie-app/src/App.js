1c8392aa904f864e104a50a1fd2991a706409b60
JKyEun <j56237@naver.com>
2023-03-21 23:45:22 +0900

    movie-app: react-router-dom 배우기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,41 +1,15 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  });
   return (
-    <div>
-      {loading ? (
-        <h1>Loading . . .</h1>
-      ) : (
-        movies.map((el) => {
-          return (
-            <div key={el.id}>
-              <h2>{el.title}</h2>
-              <img src={el.medium_cover_image} alt='영화 포스터' />
-              <p>{el.summary}</p>
-              <ul>
-                {el.genres.map((el) => (
-                  <li key={el}>{el}</li>
-                ))}
-              </ul>
-            </div>
-          );
-        })
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/movie' element={<Detail />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ movie-app/src/components/movie.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function Movie({ coverImage, title, summary, genres }) {
+  return (
+    <div>
+      <h2>
+        <Link to='/movie'>{title}</Link>
+      </h2>
+      <img src={coverImage} alt='영화 포스터' />
+      <p>{summary}</p>
+      <ul>
+        {genres.map((el) => (
+          <li key={el}>{el}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImage: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
